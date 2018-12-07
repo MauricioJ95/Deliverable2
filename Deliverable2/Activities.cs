a914a459744bec250bdae65df77129e8f6bfa0f0
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-12-07 14:31:00 -0500

    inital commit

---
+++ Deliverable2/Activities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable2
+{
+    class Activities
+    {
+        Dictionary<int, string> Actions { get; set; }
+        public int[] Num { get; set; }
+        public string[] Activitie { get; set; }
+
+        public Activities()
+        {
+            Num = new[] { 1, 2, 3, 4 };
+            Activitie = new string[] { "Stock Car Racing", "Hiking", "Skydiving", "Taco Bell" };
+            Actions = new 
+        }
+    }
+}
---
+++ Deliverable2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user, what are you in the mood for?");
+            Console.WriteLine("Here are your options: 1) Action 2) Chill times 3) Danger 4) Good food");
+            Console.ReadLine();
+
+            Console.WriteLine("How many people are you bringing with you?");
+        }
+    }
+}
