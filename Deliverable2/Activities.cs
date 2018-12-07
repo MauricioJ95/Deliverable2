1af8bc8a1b88c8c8cc1998059175d687c8806e81
Bourke, David <davidbourke@quickenloans.com>
2018-12-07 14:55:39 -0500

    input validation

--- Deliverable2/Activities.cs
+++ Deliverable2/Activities.cs
@@ -10,13 +10,23 @@ namespace Deliverable2
     {
         Dictionary<int, string> Actions { get; set; }
         public int[] Num { get; set; }
-        public string[] Activitie { get; set; }
+        public string[] Activity { get; set; }
 
         public Activities()
         {
             Num = new[] { 1, 2, 3, 4 };
-            Activitie = new string[] { "Stock Car Racing", "Hiking", "Skydiving", "Taco Bell" };
-            Actions = new 
+            Activity = new string[] { "Stock Car Racing", "Hiking", "Skydiving", "Taco Bell" };
+            Actions = new Dictionary<int, string>();
+
+            Actions.Add(1, "Stock car Racing");
+            Actions.Add(2, "Stock car Racing");
+            Actions.Add(3, "Stock car Racing");
+            Actions.Add(4, "Stock car Racing");
+        }
+
+        public bool IsValidInput(string input)
+        {
+            return Actions.ContainsKey(int.Parse(input));
         }
     }
 }
