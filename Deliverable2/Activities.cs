b5e86b40aa3c1ff91b01a1c2c26de258944d4f2e
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-12-07 15:58:35 -0500

    getting some 32bit error

--- Deliverable2/Activities.cs
+++ Deliverable2/Activities.cs
@@ -10,13 +10,50 @@ namespace Deliverable2
     {
         Dictionary<int, string> Actions { get; set; }
         public int[] Num { get; set; }
-        public string[] Activitie { get; set; }
+        public string[] Activity { get; set; }
+        public int numberofPeople { get; set; }
 
         public Activities()
         {
             Num = new[] { 1, 2, 3, 4 };
-            Activitie = new string[] { "Stock Car Racing", "Hiking", "Skydiving", "Taco Bell" };
-            Actions = new 
+            Activity = new string[] { "Stock Car Racing", "Hiking", "Skydiving", "Taco Bell" };
+            Actions = new Dictionary<int, string>
+            {
+                {1, "Stock Car Racing" },
+                {2, "Hiking" },
+                {3, "Skydiving" },
+                {4, "Taco Bell" }   
+            };
+
+        }
+
+            public void Task()
+            {
+                
+                if (numberofPeople < 1)
+                {
+                    Console.WriteLine("Okay if you're in the mood for " + (Num) + ", then you should go to " + (Activity) + "and travel in sneakers!");
+                }
+                else if (numberofPeople < 5)
+                {
+                    Console.WriteLine("Okay if you're in the mood for " + (Num) + ", then you should go to " + (Activity) + "and travel in sedan!");
+                }
+                else if (numberofPeople < 11)
+                {
+                    Console.WriteLine("Okay if you're in the mood for " + (Num) + ", then you should go to " + (Activity) + "and travel in a Volkswagen bus!");
+                }
+                else if (numberofPeople > 11)
+                {
+                    Console.WriteLine("Okay if you're in the mood for " + (Num) + ", then you should go to " + (Activity) + "and travel in an airplane!");
+                }
+                else
+                {
+                    Console.WriteLine("what is you doing?");
+                }
+            }
+        public bool IsValidInput(string input)
+        {
+            return Actions.ContainsKey(int.Parse(input));
         }
     }
 }
--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -8,13 +8,37 @@ namespace Deliverable2
 {
     class Program
     {
+        private static Activities activity;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello user, what are you in the mood for?");
-            Console.WriteLine("Here are your options: 1) Action 2) Chill times 3) Danger 4) Good food");
-            Console.ReadLine();
+            activity = new Activities();
+            string input;
+
+            var isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.WriteLine("Hello user, what are you in the mood for?");
+                Console.WriteLine("Here are your options: 1) Action 2) Chill times 3) Danger 4) Good food");
+                input = Console.ReadLine();
+
+                isValidInput = activity.IsValidInput(input);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Invalid selection, please choose from the menu");
+                }
+            }
+
 
             Console.WriteLine("How many people are you bringing with you?");
+
+            input = Console.ReadLine();
+
+            var a = new Activities();
+
+            a.Task();
+
         }
     }
 }
